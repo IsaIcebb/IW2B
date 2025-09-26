67ab5ad7cd2addcdb77fa2e180bec6f1956b8106
IsaIcebb <167563308+IsaIcebb@users.noreply.github.com>
2025-09-26 16:34:48 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,7 @@
-CREATE TABLE pets (
+CREATE TABLE pacientes (
     id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    nome_dono VARCHAR(150) NOT NULL,
+    nome VARCHAR(150) NOT NULL,
     email VARCHAR(150) NOT NULL,
     telefone VARCHAR(20) NOT NULL,
-    rg VARCHAR(20) NOT NULL,
-    nome_pet VARCHAR(100) NOT NULL,
-    idade_pet INT(3) NOT NULL,
-    raca_pet VARCHAR(100) NOT NULL
+    ativo BOOLEAN NOT NULL DEFAULT 1
 );
